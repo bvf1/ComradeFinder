25bdece3d3e9007b7d83780c6668607b0a5fe20d
JonAlex99 <jonalexpetursson@gmail.com>
2021-11-26 15:23:27 +0000

    lagaði redirect fyrir application

--- src/main/java/is/hi/comradefinder/Controllers/ApplicationController.java
+++ src/main/java/is/hi/comradefinder/Controllers/ApplicationController.java
@@ -12,7 +12,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -60,7 +59,7 @@ public class ApplicationController {
 
         applicationService.save(application);
 
-        return "makeApplication";
+        return "redirect:/user/" + user.getID();
     }
 
    /* @RequestMapping(value="/makeApplication", method = RequestMethod.GET)
